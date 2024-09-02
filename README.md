cdd99d73a57b56ae872c99ddfb3a5a3de1dd0ba7
회석 <jsk2343@naver.com>
2024-09-02 16:21:58 +0900

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">Toit</h1>
+
+Together do it! Toit
+
+## Documentation
+
+### Get Started
+```zsh
+yarn
+
+yarn build
+yarn start
+```
+
+### Node
+
+- node.js `v21.7.3`
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import '@/styles/global.css';
+
+import type { Metadata, NextPage } from 'next';
+import React from 'react';
+
+import ReactQueryProvider from '@/components/provider/ReactQueryProvider';
+
+export const metadata: Metadata = {
+    title: 'Toit',
+    description: 'Together do it! Toit',
+};
+
+const RootLayout: NextPage<React.PropsWithChildren> = (props) => {
+    const { children } = props;
+    return (
+        <html lang="ko">
+            <body>
+                <ReactQueryProvider>{children}</ReactQueryProvider>
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
