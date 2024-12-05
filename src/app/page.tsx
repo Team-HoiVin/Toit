bc6d6ff327262cd2a34894a4e1a9a58163da56b1
eunbinnie <dmsqlsdl54@gmail.com>
2024-12-05 15:48:32 +0900

    refactor: 미사용 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,11 @@
 import React from 'react';
 
-import Input from '@/components/input/Input';
 import ZustandTest from '@/components/ZustandTest';
 
 const page = () => {
   return (
     <div>
       <ZustandTest />
-      <div className='my-4 grid gap-4'>
-        <Input size='small' placeholder='size : small' />
-        <Input size='large' placeholder='size : large' />
-        <label htmlFor='error'>error</label>
-        <div>
-          <Input
-            id='error'
-            size='large'
-            placeholder='error'
-            error
-            helperText='이메일을 입력해주세요'
-          />
-        </div>
-        <Input
-          size='large'
-          placeholder='비밀번호(영문, 숫자 포함, 12자 이내)를 입력해주세요.'
-          password
-        />
-        <Input size='large' disabled placeholder='disabled' />
-        <Input size='large' placeholder='password disabled' disabled password />
-      </div>
     </div>
   );
 };
