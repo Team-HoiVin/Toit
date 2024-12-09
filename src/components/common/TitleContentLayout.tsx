19e833bac389df4607fca11a97baa876de6f8db6
eunbinnie <dmsqlsdl54@gmail.com>
2024-12-09 15:48:07 +0900

    refactor: children type 정의 수정

--- src/components/common/TitleContentLayout.tsx
+++ src/components/common/TitleContentLayout.tsx
@@ -1,6 +1,7 @@
-interface TitleContentLayoutProps {
+import type { PropsWithChildren } from 'react';
+
+interface TitleContentLayoutProps extends PropsWithChildren {
   title: string;
-  children: React.ReactNode;
 }
 
 /**
@@ -9,7 +10,7 @@ interface TitleContentLayoutProps {
  * 페이지 상단에 제목(title)을 표시하고, 그 하단에 자식 요소(children)를 렌더링하는 레이아웃 컴포넌트입니다.
  *
  * @param {string} title 페이지 타이틀로, 주 콘텐츠의 주제를 나타냅니다.
- * @param {React.ReactNode} children 타이틀 하단에 렌더링되는 콘텐츠입니다.
+ * @param {ReactNode} children 타이틀 하단에 렌더링되는 콘텐츠입니다.
  */
 const TitleContentLayout = ({ title, children }: TitleContentLayoutProps) => {
   return (
