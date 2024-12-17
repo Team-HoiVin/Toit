517fe5d2f6b0cb02f4eb77a78db3495118a415ee
정회석 <73456923+jeong-hoi-seok@users.noreply.github.com>
2024-12-17 21:55:59 +0900

    fix: 불필요한 headers 제거

--- src/app/(auth)/signup/_utils/mutation.ts
+++ src/app/(auth)/signup/_utils/mutation.ts
@@ -6,9 +6,6 @@ import type { ILoginResponse } from '../../login/_types/login.interface';
 export const signUpMutationFn = async (data: ISignup) => {
   const res = await fetch('/api/auth/signUp', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(data),
   });
 
