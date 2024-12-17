3187a981adec57adc17131ff374401921a86b6c5
정회석 <73456923+jeong-hoi-seok@users.noreply.github.com>
2024-12-17 18:15:38 +0900

    feat: middleware 수정 (#18)

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/user/:path*',
-        destination: `${process.env.API_URL}/auth/:path*`,
-      },
-      {
-        source: '/groups/:path*',
-        destination: `${process.env.API_URL}/groups/:path*`,
-      },
-      {
-        source: '/oauthApps',
-        destination: `${process.env.API_URL}/oauthApps`,
-      },
-      {
-        source: '/images',
-        destination: `${process.env.API_URL}/images`,
-      },
-      {
-        source: '/tasks/:path*',
-        destination: `${process.env.API_URL}/tasks/:path*`,
-      },
-      {
-        source: '/auth/:path*',
-        destination: `${process.env.API_URL}/auth/:path*`,
-      },
-      {
-        source: '/articles/:path*',
-        destination: `${process.env.API_URL}/articles/:path*`,
-      },
-      {
-        source: '/comments/:path*',
-        destination: `${process.env.API_URL}/comments/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
